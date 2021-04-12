25844f6e073d17dbd5db901c9821eeed1a6b98fa
Liz <liz.muehlmann@uci.edu>
2021-04-12 12:25:48 -0700

    Minor spelling and formatting changes.

---
+++ ISL Machine Learning/exercises/ISLch2exercises.r
@@ -0,0 +1,9 @@
+## You'll have to load the libraries library("ISLR") and library("MASS"). The latter package needs to be installed first using install.packages("MASS"). You only need to install the package once.
+
+## load libraries
+library("MASS")
+library("ISLR")
+
+## load data
+## since we're using data stored in an R package and not a CSV, you need to store the data in a variable to call on later
+print("College")

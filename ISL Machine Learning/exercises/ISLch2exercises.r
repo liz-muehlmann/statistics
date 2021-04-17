ff54a8aebf691e0ccef0d7ed103feec3e43e039e
Liz <Liz.muehlmann@uci.edu>
2021-04-17 12:40:22 -0700

    Reorganized files, added chapter 3 excercises

--- ISL Machine Learning/exercises/ISLch2exercises.r
+++
@@ -1,9 +0,0 @@
-## You'll have to load the libraries library("ISLR") and library("MASS"). The latter package needs to be installed first using install.packages("MASS"). You only need to install the package once.
-
-## load libraries
-library("MASS")
-library("ISLR")
-
-## load data
-## since we're using data stored in an R package and not a CSV, you need to store the data in a variable to call on later
-print("College")
